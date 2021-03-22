ec1637cbaf52453e340a6058eb008112c6c96ab6
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-03-22 09:52:28 +0100

    Add coordinate classifier

--- classifier.py
+++ classifier.py
@@ -29,11 +29,7 @@ class Classifier:
         y_pred = self.predict(X)
         return metric.evaluate(y_true, y_pred)
 
-    def _train_iteration(self, X, y):
-        p = self._predict(X)
-        self.beta += self._compute_derivative(X, y, p)
-
-    def _compute_derivative(self, X, y, p):
+    def _train_outer_iteration(self, X, y):
         pass
 
     def _predict(self, X):
@@ -48,14 +44,16 @@ class Classifier:
         return (np.log(y_pred_proba).T @ y_true + np.log(1 - y_pred_proba).T @ (1 - y_true))[0, 0]
 
 
-class IRLS(Classifier):
+class CoordinateClassifier(Classifier):
 
     def __init__(self, eps=0.001, **kwargs):
         super().__init__(**kwargs)
         self.eps = eps
 
-    def _compute_derivative(self, X, y, p):
-        W = np.diag([x*(1-x) + self.eps for x in p.reshape(-1)])
-        return np.linalg.inv(X.T @ W @ X) @ X.T @ (y.reshape((-1, 1)) - p.reshape(-1, 1))
+    def _train_outer_iteration(self, X, y):
+        for i in range(X.shape[1]):
+            self._train_inner_iteration(X, y)
 
+    def _train_inner_iteration(self, X, y):
+        pass
 
