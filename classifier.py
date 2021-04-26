6734cea161dc6633d44a33cc93470b7e517da720
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-04-26 23:42:03 +0200

    Implement coordinate descent and permuted version

--- classifier.py
+++ classifier.py
@@ -4,40 +4,74 @@ import numpy as np
 
 
 class Classifier:
-    def __init__(self, **kwargs):
+    def __init__(self, C=1, **kwargs):
+        self.C = C
         self.stopper = Stopper(**kwargs)
-        self.beta = None
+        self.w = None
+        self.n = None
+        self.p = None
+        self.X = None
+        self.y = None
 
     def fit(self, X, y):
+        X = self._add_ones(X)
         y = y.reshape(-1, 1)
-        self.beta = np.zeros([X.shape[1], 1])
-        while not self.stopper.stop(self):
-            self._train_outer_iteration(X, y)
-            y_pred_proba = self._predict(X)
-            self.log_likelihood.append(self._log_likelihood(y, y_pred_proba))
+        self.n = y.shape[0]
+        self.p = X.shape[1]
+        self.w = np.zeros([self.p, 1])
+        self.X = X
+        self.y = y
+        while not self.stopper.stop():
+            self._train_outer_iteration()
+            # y_pred_proba = self._predict(X)
+            # self.log_likelihood.append(self._log_likelihood(y, y_pred_proba))
         return self
 
+    # not implemented
+    @FutureWarning
     def predict_proba(self, X):
         return self._predict(X).reshape(-1)
 
+    # not implemented
+    @FutureWarning
     def predict(self, X, threshold=0.5):
         y_pred = np.zeros(X.shape[0])
         y_pred[self._predict(X).reshape(-1) > threshold] = 1
         return y_pred
 
+    # not implemented
+    @FutureWarning
     def score(self, X, y_true, metric: Metric):
         y_pred = self.predict(X)
         return metric.evaluate(y_true, y_pred)
 
-    def _train_outer_iteration(self, X, y):
+    def _train_outer_iteration(self):
         pass
 
+    # TODO how to predict probabilities in SVM? since _predict method should return probabilities
+    # for now method returns classes
     def _predict(self, X):
         """
         :param X: matrix with observations: n_observations x n_predictors
         :return: predictions as np.array n_observations x 1
         """
-        return 1 / (1 + np.exp(-X @ self.beta))
+        res = np.sign(self._add_ones(X) @ self.w)
+        res = res.reshape(-1)
+        res[res == 0] = 1  # should be rare unless w==0
+        return res
+
+    def _add_ones(self, X):
+        ones = np.ones([X.shape[0], 1])
+        return np.concatenate([ones, X], axis=1)
+
+    def L2_SVM_loss(self, w=None):
+        if w is None:
+            w = self.w
+        loss = 0
+        for i in range(self.n):
+            loss += max(1 - self.y[i, 0] * self.X[i, :] @ w, 0) ** 2
+        loss *= self.C
+        return loss + w.T @ w / 2
 
     @staticmethod
     def _log_likelihood(y_true, y_pred_proba):
@@ -46,13 +80,60 @@ class Classifier:
 
 class CoordinateClassifier(Classifier):
 
+    def __init__(self, sigma=0.01, Beta=0.5, **kwargs):
+        self.sigma = sigma
+        self.Beta = Beta
+        super().__init__(**kwargs)
+
+    def _train_outer_iteration(self):
+        print(f'Iteration {self.stopper.n_iter}')
+        for i in range(self.p):
+            self._train_inner_iteration(i)
+
+    def _train_inner_iteration(self, i):
+        print(f'w={self.w} at beginning of changing coordinate {i}')
+        d = - self.compute_D_derivative(i, 0) / self.compute_D_second_derivative(i, 0)
+        lambda_ = 1
+        while not self.optimally_constraint_12(i, lambda_*d):
+            lambda_ *= self.Beta
+        self.w[i, 0] += lambda_*d
+
+    def compute_D(self, i, z):
+        e = np.zeros([self.p, 1])
+        e[i, 0] = 1
+        return self.L2_SVM_loss(self.w + z*e)
+
+    def compute_D_derivative(self, i, z):
+        e = np.zeros([self.p, 1])
+        e[i, 0] = 1
+        w = self.w + z*e
+        derivative = 0
+        for j in range(self.n):
+            derivative += max(1 - self.y[j, 0] * self.X[j, :] @ w, 0) * self.y[j] * self.X[j, i]
+        derivative *= (-2 * self.C)
+        return derivative + w[i, 0]
+
+    def compute_D_second_derivative(self, i, z):
+        e = np.zeros([self.p, 1])
+        e[i, 0] = 1
+        w = self.w + z * e
+        derivative = 0
+        for j in range(self.n):
+            derivative += np.sign(max(1 - self.y[j, 0] * self.X[j, :] @ w, 0)) * (self.X[j, i] ** 2)
+        derivative *= (2 * self.C)
+        return derivative + 1
+
+    # constraint 12 from paper
+    def optimally_constraint_12(self, i, z):
+        return self.compute_D(i, z) - self.compute_D(i, 0) <= - self.sigma * (z**2)
+
+
+class PermutedCoordinateClassifier(CoordinateClassifier):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def _train_outer_iteration(self, X, y):
-        for i in range(X.shape[1]):
-            self._train_inner_iteration(X, y)
-
-    def _train_inner_iteration(self, X, y):
-        pass
-
+    def _train_outer_iteration(self):
+        print(f'Iteration {self.stopper.n_iter}')
+        coordinate = np.random.randint(0, self.p)
+        self._train_inner_iteration(coordinate)
