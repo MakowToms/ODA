852f300b17442a31120ec06e8a63f2a8a7f3523f
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-06-08 21:52:03 +0200

    add stop by loss

--- stopper.py
+++ stopper.py
@@ -1,17 +1,25 @@
+import numpy as np
+
 
 class Stopper:
     def __init__(self, max_iter: int = 10, **kwargs):
         self.max_iter = max_iter
         self.n_iter = 0
         self.kwargs = kwargs
+        self.last_loss = np.inf
 
     def new_training(self):
         self.n_iter = 0
 
-    def stop(self, **kwargs) -> bool:
+    def stop(self, classifier, **kwargs) -> bool:
+        print(f'Iteration: {self.n_iter}')
         kwargs.update(self.kwargs)
         self.n_iter += 1
+        loss = classifier.L2_SVM_loss()[0][0]
+        print(loss)
         if self.n_iter >= self.max_iter:
             return True
+        if (self.last_loss - loss) / loss < 1e-3:
+            return True
+        self.last_loss = loss
         return False
-
